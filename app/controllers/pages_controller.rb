8ebb96c4ce856496a61b34db4d9e6c692ad55140
Nithin Bharadhwaj <anibha88@gmail.com>
2018-03-31 21:33:13 +0530

    added some query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
   def home
+  	if params[:city].present?
+  		city = params[:city]
+	  	office = params[:office]
+	  	building = params[:building]
+	  	building = building.split(",").first rescue nil
+	  	floor = 1
+	  	@results = Ipm.where(organisation: "Wipro", city: city, office: office, building: building, floor: 1)
+	  	# render json: @results
+	  	
+	  	render_to_string @results # pages_result_path(:results => @results)
+	  	return
+	end
   end
 
   def about
@@ -7,4 +19,20 @@ class PagesController < ApplicationController
 
   def help
   end
+
+  
+
+  def result
+	# city = params[:city]
+ #  	office = params[:office]
+ #  	building = params[:building]
+ #  	building = building.split(",").first rescue nil
+ #  	floor = 1
+ #  	@results = Ipm.where(organisation: "Wipro", city: city, office: office, building: building, floor: 1)
+  	
+  end
+
+  
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # get 'pages/home'
+  
+  get 'pages/result'
 
   get 'pages/about'
 
   get 'pages/help'
 
+  
+
   resources :ipms
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
